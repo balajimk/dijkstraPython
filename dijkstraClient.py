edcef9e7319bfef2a66821e3148f4052b537c75c
Balaji Kala <bkala@sgfleet.com>
2020-08-02 23:34:00 +1000

    imports included

--- dijkstraClient.py
+++ dijkstraClient.py
@@ -1,25 +1,27 @@
-import dijkstra
+from dijkstra import Vertex
+from dijkstra import Edge
+from dijkstra import calculateShortestPath
 
 # code here
 vertices = []
-vertices.append(dijkstra.Vertex('A'))
-vertices.append(dijkstra.Vertex('B'))
-vertices.append(dijkstra.Vertex('C'))
-vertices.append(dijkstra.Vertex('D'))
-vertices.append(dijkstra.Vertex('E'))
+vertices.append(Vertex('A'))
+vertices.append(Vertex('B'))
+vertices.append(Vertex('C'))
+vertices.append(Vertex('D'))
+vertices.append(Vertex('E'))
 
 edges = []
-edges.append(dijkstra.Edge('A', 'B', 6))
-edges.append(dijkstra.Edge('A', 'D', 1))
-edges.append(dijkstra.Edge('B', 'C', 5))
-edges.append(dijkstra.Edge('D', 'B', 2))
-edges.append(dijkstra.Edge('E', 'B', 2))
-edges.append(dijkstra.Edge('C', 'E', 5))
-edges.append(dijkstra.Edge('D', 'E', 1))
+edges.append(Edge('A', 'B', 6))
+edges.append(Edge('A', 'D', 1))
+edges.append(Edge('B', 'C', 5))
+edges.append(Edge('D', 'B', 2))
+edges.append(Edge('E', 'B', 2))
+edges.append(Edge('C', 'E', 5))
+edges.append(Edge('D', 'E', 1))
 
 startVertex = 'A'
 endVertex = 'C'
-verticesResult = dijkstra.calculateShortestPath(vertices, edges, startVertex, endVertex)
+verticesResult = calculateShortestPath(vertices, edges, startVertex, endVertex)
 
 # print result
 resultVertex = verticesResult.where(lambda v: v.name == startVertex)[0]
